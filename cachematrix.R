1cfb3b4d99f8b21cde5939a6b6af0480a85c142a
dburrdoss <dburrdoss@gmail.com>
2015-05-24 12:27:10 -0400

    adding my matrix inverse code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions makeCacheMatrix and cacheSolve create a special object that stores
+## a square invertible matrix and computes (if necessary) and stores its inverse.
+## Note the functions assume the user-supplied matrix is square and invertible.
+##
 
-## Write a short comment describing this function
+## The first function, makeCacheMatrix, creates a special list object
+## which contains four functions to:
+##   set the value of the matrix
+##   get the value of the matrix
+##   set the value of the inverse of the matrix
+##   get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     xinv <- NULL
+     set <- function(y){
+         x <<- y
+         xinv <<- NULL
+     }
+     get <- function() x
+     setinv <- function(inv) xinv <<- inv
+     getinv <- function() xinv
+     list(set = set, get = get,
+          setinv = setinv,
+          getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The second function, cacheSolve, calculates the inverse of the
+## matrix in the special object created by makeCacheMatrix.  However,
+## it first checks to see if the inverse has already been calculated.
+## If so, it gets the inverse from the cache and skips the
+## computation.  Otherwise, it calculates the inverse of the matrix
+## and sets the value of the inverse in the cache via the setmean
+## function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    xinv <- x$getinv()
+    if(!is.null(xinv)) {
+        message("getting cached data")
+        return(xinv)
+     }
+    data <- x$get()
+    xinv <- solve(data, ...)
+    x$setinv(xinv)
+    xinv
 }
+
+
+# An illustration:
+amat <- matrix(c(0,1,1,1),nrow=2)
+x <- makeCacheMatrix(amat)
+cacheSolve(x)
+# run it a second time; same inverse but from the cache:
+cacheSolve(x)
+# Note:  Errors result if you name the original matrix 'x' in the above code
